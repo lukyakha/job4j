2576fc904c152c7e5c750a4525a7fc77452bdf0d
lukyakha <lukyakha@mail.ru>
2019-07-30 06:32:34 +0300

    4.1. Глупый бот.[#153470]

--- chapter_001/src/test/java/ru/job4j/condition/DummyBotTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -5,7 +5,7 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
- * @author Petr Arsentev (parsentev@yandex.ru)
+ * @author Semyon Lukyanov
  * @version $Id$
  * @since 0.1
  */
